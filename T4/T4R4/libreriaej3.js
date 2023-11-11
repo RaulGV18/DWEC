62e8e912ab308c2f77a3dfb97166a7ca71b79188
RaulGV18 <rgalval1806@g.educaand.es>
2023-11-11 18:55:49 +0100

    commit

--- T4/T4R4/libreriaej3.js
+++ T4/T4R4/libreriaej3.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('username').onchange = function (){
         var username = document.getElementById('username').value;
       
-        var lowercaseRegex = "[a-z]";
-        var uppercaseRegex = "[A-Z]";
-        var minLengthRegex = "{6,}";
+        var lowercaseRegex = /[a-z]/;
+        var uppercaseRegex = /[A-Z]/;
+        var minLengthRegex = /[A-Za-z0-9]{6,}/;
       
         var lowercaseTest = lowercaseRegex.test(username);
         var uppercaseTest = uppercaseRegex.test(username);
